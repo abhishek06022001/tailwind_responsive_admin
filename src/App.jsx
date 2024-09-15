71a18caca2306ce129ba06e7f4f855524a921a37
Abhishek <satheabhishek1@gmail.com>
2024-09-15 15:20:22 +0530

    Revert "first commit"
    This reverts commit 10cd7f73213ce2306ecdd4a18f696924cc954189.

--- src/App.jsx
+++ src/App.jsx
@@ -1,22 +1,10 @@
 import React from 'react'
-import { BrowserRouter, Routes, Route } from "react-router-dom";
-import Layout from './components/Layout';
-import Home from './components/Home';
-import About from './components/About';
-import Student from './components/Student';
-import Order from './components/Order';
+
 function App() {
   return (
-    <BrowserRouter>
-      <Routes>
-        <Route path="/" element={<Layout />}  >
-          <Route index element={<Home />} />
-          <Route path='about' element={<About />} />
-          <Route path='student' element={<Student />} />
-          <Route path='order' element={<Order />} />
-        </Route>
-      </Routes>
-    </BrowserRouter>
+    <>
+      <h1>React Tailwind Template :) </h1>
+    </>
   )
 }
 
