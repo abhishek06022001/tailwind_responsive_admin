10cd7f73213ce2306ecdd4a18f696924cc954189
Abhishek <satheabhishek1@gmail.com>
2024-09-15 15:17:12 +0530

    first commit

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,22 @@
 import React from 'react'
-
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Layout from './components/Layout';
+import Home from './components/Home';
+import About from './components/About';
+import Student from './components/Student';
+import Order from './components/Order';
 function App() {
   return (
-    <>
-      <h1>React Tailwind Template :) </h1>
-    </>
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Layout />}  >
+          <Route index element={<Home />} />
+          <Route path='about' element={<About />} />
+          <Route path='student' element={<Student />} />
+          <Route path='order' element={<Order />} />
+        </Route>
+      </Routes>
+    </BrowserRouter>
   )
 }
 
